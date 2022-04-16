36d77c729f3a274a3cdcd5f8138cc180c7892f90
ChihYu Yeh <chihyu.jimmy.yeh@gmail.com>
2022-04-16 21:26:30 +0800

    add ch1 programs using js

---
+++ ch1/cyyeh/js/refactored/main.js
@@ -0,0 +1,40 @@
+import {
+  statement 
+} from './statement.js';
+
+/** 
+ * plays.json
+ */
+const plays = {
+  "hamlet": {"name": "Hamlet", "type": "tragedy"},
+  "as-like": {"name": "As You Like It", "type": "comedy"},
+  "othello": {"name": "Othello", "type": "tragedy"}
+}
+
+
+/**
+ * invoices.json
+ */
+const invoices = [
+  {
+    "customer": "BigCo",
+    "performances": [
+      {
+        "playID": "hamlet",
+        "audience": 55
+      },
+      {
+        "playID": "as-like",
+        "audience": 35
+      },
+      {
+        "playID": "othello",
+        "audience": 40
+      }
+    ]
+  }
+]
+
+for (const invoice of invoices) {
+  console.log(statement(invoice, plays))
+}
